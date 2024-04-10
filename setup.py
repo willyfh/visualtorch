c658576baa8dd7996e50ff72ce1244633d985d97
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-04-11 08:27:33 +0900

    Bump version (#41)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with file_path.open("r") as fh:
 
 setuptools.setup(
     name="visualtorch",
-    version="0.2.2",
+    version="0.2.3",
     author="Willy Fitra Hendria",
     author_email="willyfitrahendria@gmail.com",
     description="Architecture visualization of Torch models",
